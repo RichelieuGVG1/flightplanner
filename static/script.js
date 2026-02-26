b5ce24577accd24ccd03265dd3cfd02a12794c60
RichelieuGVG1 <uryanovvasya@gmail.com>
2026-02-26 12:46:58 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -328,9 +328,9 @@ const planeLayer = new ol.layer.Vector({
 map.addLayer(planeLayer);
 
 // Plane Icon (Simple SVG Data URI)
-const planeSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="%2338bdf8" width="48px" height="48px"><path d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z"/></svg>`;
-const planeIconUrl = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(planeSvg.replace('%2338bdf8', '#0ea5e9'));
-
+const planeSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#38bdf8" width="48px" height="48px"><path stroke="#000" stroke-width="0.3" d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z"/></svg>`;
+const planeIconUrl = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(planeSvg.replace('#38bdf8', '#85a2afff'));
+//цвет встречного самолета
 const planeStyle = new ol.style.Style({
     image: new ol.style.Icon({
         src: planeIconUrl,
